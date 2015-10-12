4fa740e3413a9899a8f957392b91a9a1ae9a8d7f
twisted1919 <totyoserbangeorgecristian@yahoo.com>
2015-10-12 23:05:33 +0300

    added php example

---
+++ examples/php/index.php
@@ -0,0 +1,30 @@
+<?php
+
+// make sure we show any error/warning/notice
+error_reporting(-1);
+ini_set('display_errors', 1);
+
+function checkIpAddress($ipAddress) {
+
+    if (!filter_var($ipAddress, FILTER_VALIDATE_IP)) {
+        throw new Exception("Please prvide a valid ip address!");
+    }
+
+    $ch = curl_init('http://127.0.0.1:8000/check/' . $ipAddress);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+
+    curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+    // if password is needed:
+    // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: yourpassword'));
+
+    $body = curl_exec($ch);
+    curl_close($ch);
+
+    return (array)json_decode($body, true);
+}
+
+print_r(checkIpAddress('123.123.123.123'));
